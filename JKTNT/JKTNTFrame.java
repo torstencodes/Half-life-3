9bd4cf9f64e7ac5b8447a85572b034e8fb76e732
Kai <61212948+gnaw6403400@users.noreply.github.com>
2020-04-05 15:25:30 -0400

    Update JKTNTFrame.java

--- JKTNT/JKTNTFrame.java
+++ JKTNT/JKTNTFrame.java
@@ -7,13 +7,16 @@ import javax.swing.*;
 public class JKTNTFrame extends JFrame {
 
 	private JLabel label;
+	private JKTNTFrame frame;
 	private JKTNTPanel mainScreen;
 	private JButton search;
+	private JButton clear;
 	private JButton login;
 	private TextField text;
+	private TextField user;
+	private TextField pass;
 	private btnListener clickListener;
-	
-	
+
 	public static void main(String[] args) {
 		new JKTNTFrame();
 	}
@@ -23,12 +26,12 @@ public class JKTNTFrame extends JFrame {
 		super("JKTNT Window");
 
 		// Line of code to set the size and location
-		
+
 		this.setSize(new Dimension(500, 500));
 		this.setLocationRelativeTo(null); // starts center screen
 		this.setLayout(new BorderLayout());
 		clickListener = new btnListener();
-		//this.setResizable(false);
+		// this.setResizable(false);
 		// Line of code that says what happens when you click close
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -55,7 +58,7 @@ public class JKTNTFrame extends JFrame {
 		// loadData.addActionListener(buttonListener);
 		// saveData.addActionListener(buttonListener);
 		// clear.addActionListener(buttonListener);
-		
+
 		// These anonymous JButtons should be replaced with instance variables
 		top.add(label);
 		top.setVisible(true);
@@ -68,22 +71,32 @@ public class JKTNTFrame extends JFrame {
 	private void setupMiddlePanel() {
 		mainScreen = new JKTNTPanel();
 		mainScreen.setLayout(new FlowLayout());
-		
-		//creates a empty 20px wide textField 
-		text = new TextField("",20);
+
+		// creates a empty 20px wide textField
+		text = new TextField("", 20);
 		mainScreen.add(text);
-		
-		//creates a Search button with event listener attached
+
+		// creates a Search button with event listener attached
 		search = new JButton("Search");
 		mainScreen.add(search);
 		search.addActionListener(clickListener);
+
+		// creates a button to clear the search bar
+		clear = new JButton("Clear");
+		mainScreen.add(clear);
+		clear.addActionListener(clickListener);
 		
-		//creates a Login button with event listener attached
+		// creates the username and password text fields
+		user = new TextField("", 20);
+		pass = new TextField("", 20);
+		mainScreen.add(user);
+		mainScreen.add(pass);
+
+		// creates a Login button with event listener attached
 		login = new JButton("Login");
 		mainScreen.add(login);
 		login.addActionListener(clickListener);
 
-		
 		mainScreen.setVisible(true);
 		// This anonymous GraphicsPanel should be replaced with an instance variable
 		this.add(mainScreen, BorderLayout.CENTER);
@@ -96,33 +109,31 @@ public class JKTNTFrame extends JFrame {
 		// Now that the panel is set up, add it to the frame
 		this.add(bottom, BorderLayout.SOUTH);
 	}
-	
+
 //	private String search() {
 //		System.out.println("lol");
 //		return "";
 //	}
-	
-	//button listener for Search, Login, etc.
-	public class btnListener implements ActionListener{
 
+	// button listener for Search, Login, etc.
+	public class btnListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			
+
 			Object btn = e.getSource();
-			
-			if(btn == search) {
-				//search for the keyword typed in the textField
+
+			if (btn == search) {
+				// search for the keyword typed in the textField
 				mainScreen.searchGames(text.getText());
-				
-			}else if(btn == login) {
-				//do nothing yet
-				mainScreen.setBackground(new Color(0,0,0));
-				System.out.println("login");
+
+			} else if (btn == login) {
+				// do nothing yet
+				mainScreen.setBackground(new Color(0, 0, 0));
 			}
-			
+
 			repaint();
 		}
-		
+
 	}
 
 }
