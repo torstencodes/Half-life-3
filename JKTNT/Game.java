3231be3ec8e27109fc82269bbb49a0647a433d5f
Kai <61212948+gnaw6403400@users.noreply.github.com>
2020-04-05 16:45:18 -0400

    Create Game.java

---
+++ JKTNT/Game.java
@@ -0,0 +1,31 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.print.DocFlavor.URL;
+import javax.swing.*;
+
+/*
+ * This is how we draw graphics on an object.
+ * We want to draw things on a JButton, so:
+ * 1. Extend JButton
+ * 2. Override paintComponent 
+ * 3. Draw our graphics in the paintComponent method
+ */
+
+public class Game extends JPanel {
+	private Image image;
+
+	public Game(String text) {
+		super();
+		image = new ImageIcon("/201GUI/deadIsland.png").getImage();
+	}
+	
+	@Override // This method already exists in JButton
+	public void paintComponent(Graphics g) {
+	    super.paintComponent(g); //Tells the JButton to do its own drawing
+
+		// Starting here, draw our graphics
+		// All graphics get drawn by starting with g.
+		g.drawImage(image, 50, 50, this);
+
+	}
+}
